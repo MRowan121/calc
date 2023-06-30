16059d76467101b3868e0857be1173bbb4feb42a
Matt Rowan <matt.rowan121@gmail.com>
2023-06-30 10:56:22 -0400

    Remove unused files and add styling to app

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Pad from "./Pad";
+
+function App() {
+  return (
+    <main className="flex flex-col justify-center items-center min-h-screen">
+      <section className="w-96">
+        <Pad />
+      </section>
+    </main>
+  );
+}
+
+export default App;
--- src/components/App/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.css";
-import Pad from "../Pad";
-
-function App() {
-  return <Pad />;
-}
-
-export default App;
