2fbbc1f8214bb9998687236e0836d82a94afac1a
Matt Rowan <matt.rowan121@gmail.com>
2023-06-30 10:41:46 -0400

    Create button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <td className="border-red-400 border-2 h-32 w-24">
+      <button className="w-full h-full text-2xl"></button>
+    </td>
+  );
+};
+
+export default Button;
